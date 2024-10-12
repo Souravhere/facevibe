f7f9beda90d3e28fdf7822cc05b5a605f66b6a39
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-12 13:43:35 +0530

    basic modal and file structure created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      welcome to face vibe
+      <h1 className="text-center my-4"> welcome to face vibe</h1>
+      <div className="px-4"> 
+      <Link href='/facedetection'
+      className="inline-block bg-gray-600 px-3 hover:bg-gray-700 duration-500 py-1 rounded-lg"
+      > Face Detection and Mood Emoji</Link>
+      </div>
     </div>
   );
 }
